39695b9dfc2791b8325a1f3f2a8a214642ce2f5a
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-05-30 00:22:57 +0500

    Org-Mode Colors

--- loveisanillusion.emacs-theme.el
+++ loveisanillusion.emacs-theme.el
@@ -5,7 +5,7 @@
 (deftheme loveisanillusion.emacs "SJ the Sahil Joseph's Love is an Illusion for Emacs.")
 
 (let (
-		 (main_foreground_color "#c6c6c6") ;; General Text Color
+		 (main_foreground_color "#c6c6c6")
 		 (secondary_foreground_color "#999999")
 		 (main_keyword_color "#ff9900")
 		 (main_strings_color "#26d726")
@@ -60,6 +60,30 @@
 		`(dired-perm-write ((t (:foreground ,main_foreground_color))))
 		`(dired-symlink ((t (:foreground ,main_comment_color))))
 		`(dired-warning ((t (:foreground ,main_foreground_color))))
+
+		
+		`(org-level-1 ((t (:foreground "DodgerBlue1" :weight bold :height 1.5))))
+		`(org-level-2 ((t (:foreground "SteelBlue1" :weight bold :height 1.4))))
+		`(org-level-3 ((t (:foreground "DeepSkyBlue1" :weight bold :height 1.3))))
+		`(org-level-4 ((t (:foreground "SkyBlue1" :weight bold :height 1.2))))
+		`(org-level-5 ((t (:foreground "LightSkyBlue1" :weight bold :height 1.1))))
+		`(org-level-6 ((t (:foreground "LightBlue1" :weight bold :height 1.0))))
+		`(org-level-7 ((t (:foreground "LightCyan1" :weight bold :height 1.0))))
+		`(org-level-8 ((t (:foreground "AliceBlue" :weight bold :height 1.0))))
+		`(org-todo ((t (:foreground "Red" :weight bold))))
+		`(org-done ((t (:foreground "ForestGreen" :weight bold))))
+		`(org-block ((t (:foreground "LightSteelBlue" :background "Gray15" :extend t))))
+		`(org-block-begin-line ((t (:foreground "LightSkyBlue" :background "Gray20" :extend t))))
+		`(org-block-end-line ((t (:foreground "LightSkyBlue" :background "Gray20" :extend t))))
+		`(org-link ((t (:foreground "Cyan1" :underline t))))
+		`(org-special-keyword ((t (:foreground "LightSalmon"))))
+		`(org-date ((t (:foreground "LightGoldenrod" :underline t))))
+		`(org-agenda-date ((t (:foreground "MediumPurple1" :weight bold))))
+		`(org-agenda-date-weekend ((t (:foreground "MediumPurple3" :weight bold))))
+		`(org-agenda-date-today ((t (:foreground "MediumPurple1" :weight bold))))
+		`(org-agenda-done ((t (:foreground "PaleGreen4"))))
+
+		
 		
 		`(eshell-prompt ((t (:foreground ,main_comment_color)))))
 	)
