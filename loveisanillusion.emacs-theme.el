fe64a2e8ad527f64e257bfc3719e3a0af70726be
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-07-09 23:47:38 +0500

    simplify

--- loveisanillusion.emacs-theme.el
+++ loveisanillusion.emacs-theme.el
@@ -8,21 +8,10 @@
 
 
 	     (main_foreground_color "#cccccc")
-         (secondary_foreground_color "#ffffff")
-	     (main_keyword_color "#cccccc")
-         (main_strings_color "#cccccc")
 	     (main_comment_color "#00acfc")
-	     (main_preprocessor_color "#cccccc")
 	     (main_background_color "#0c0c0c")
 	     (main_highlight_color "#1a0000")
-	     (main_modeline_foreground_color "#0c0c0c")
-	     (main_modeline_background_color "#cccccc")
-	     (main_modeline_inactive_foreground_color "#cccccc")
-	     (main_modeline_inactive_background_color "#0c0c0c")
 	     (main_iSearch_color "#003a3a")
-	     (pure_black_color "#000000")
-	     (pure_white_color "#ffffff")
-	     (pure_red_color "#ff0000")
 	     (main_hl_line_background_color "#003300")
  	     (main_line_number_color "#555555")
  	     (main_line_number_current_line_color "#00acfc")
@@ -36,26 +25,26 @@
 		`(default ((t (:foreground ,main_foreground_color :background ,main_background_color))))
 		`(font-lock-comment-face ((t (:foreground ,main_comment_color))))
 		`(font-lock-doc-face ((t (:foreground ,main_foreground_color))))
-		`(font-lock-string-face ((t (:foreground ,main_strings_color))))
-		`(font-lock-keyword-face ((t (:foreground ,main_keyword_color))))
+		`(font-lock-string-face ((t (:foreground ,main_foreground_color))))
+		`(font-lock-keyword-face ((t (:foreground ,main_foreground_color))))
 		`(font-lock-variable-name-face ((t (:foreground ,main_foreground_color))))
-		`(font-lock-builtin-face ((t (:foreground ,main_keyword_color))))
-		`(font-lock-constant-face ((t (:foreground ,main_strings_color))))
-	    `(font-lock-number-face ((t (:foreground ,main_strings_color))))
-		`(font-lock-type-face ((t (:foreground ,main_keyword_color))))
-		`(font-lock-preprocessor-face ((t (:foreground ,main_preprocessor_color))))
+		`(font-lock-builtin-face ((t (:foreground ,main_foreground_color))))
+		`(font-lock-constant-face ((t (:foreground ,main_foreground_color))))
+	    `(font-lock-number-face ((t (:foreground ,main_foreground_color))))
+		`(font-lock-type-face ((t (:foreground ,main_foreground_color))))
+		`(font-lock-preprocessor-face ((t (:foreground ,main_foreground_color))))
 		`(font-lock-function-name-face ((t (:foreground ,main_foreground_color))))
      	`(font-lock-function-call-face ((t (:foreground ,main_foreground_color))))
 
-		`(mode-line ((t (:foreground ,main_modeline_foreground_color :background ,main_modeline_background_color))))
-		`(mode-line-inactive ((t (:foreground ,main_modeline_inactive_foreground_color :background ,main_modeline_inactive_background_color))))
+		`(mode-line-inactive ((t (:foreground ,main_foreground_color :background ,main_background_color))))
+		`(mode-line ((t (:foreground ,main_background_color :background ,main_foreground_color))))
 
 		`(show-paren-match ((nil )))
         `(show-paren-mismatch ((nil )))
 
 		`(region ((t (:background ,main_highlight_color :foreground ,main_foreground_color))))
 		
-		`(minibuffer-prompt ((t (:foreground ,pure_red_color))))
+		`(minibuffer-prompt ((t (:foreground ,main_comment_color))))
 
 		`(isearch ((t (:background ,main_iSearch_color :foreground ,main_foreground_color))))
 		`(query-replace ((t (:background ,main_iSearch_color :foreground ,main_foreground_color))))
@@ -63,9 +52,9 @@
 		`(escape-glyph ((t (:background ,main_iSearch_color :foreground ,main_foreground_color))))
 
 		`(dired-directory ((t (:foreground ,main_background_color :background ,main_foreground_color))))
-		`(dired-flagged ((t (:foreground ,secondary_foreground_color))))
+		`(dired-flagged ((t (:foreground ,main_foreground_color))))
 		`(dired-header ((t (:foreground ,main_foreground_color))))
-		`(dired-ignored ((t (:foreground ,secondary_foreground_color))))
+		`(dired-ignored ((t (:foreground ,main_foreground_color))))
 		`(dired-marked ((t (:foreground ,main_comment_color))))
 		`(dired-perm-write ((t (:foreground ,main_foreground_color))))
 		`(dired-symlink ((t (:foreground ,main_comment_color))))
@@ -93,7 +82,6 @@
 		`(org-agenda-date-today ((t (:foreground,main_foreground_color :weight bold))))
 		`(org-agenda-done ((t (:foreground,main_foreground_color))))
 		
-		`(completions-highlight ((t (:foreground ,main_foreground_color :background ,main_highlight_color))))
 		`(highlight ((t (:foreground ,main_foreground_color :background ,main_highlight_color))))
 		`(hl-line ((t (:foreground ,main_foreground_color :background ,main_hl_line_background_color))))
 		
@@ -101,7 +89,7 @@
 		`(line-number-current-line ((t (:foreground ,main_line_number_current_line_color :background ,main_background_color))))
 
 		
-		`(eshell-prompt ((t (:foreground ,main_strings_color)))))
+		`(eshell-prompt ((t (:foreground ,main_foreground_color)))))
 	)
 
 (provide-theme 'loveisanillusion.emacs)
