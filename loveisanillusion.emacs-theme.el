d62848d11cf7f6c131fa8f850b52e1fe68c9aa16
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-07-27 21:13:47 +0500

    ... new start but with problems

--- loveisanillusion.emacs-theme.el
+++ loveisanillusion.emacs-theme.el
@@ -5,94 +5,67 @@
 (deftheme loveisanillusion.emacs "SJ the Sahil Joseph's Love is an Illusion for Emacs.")
 
 (let (
-
-
-		 ;;		 (main_foreground_color "#000000")
-		 ;;		 (main_comment_color "#008a00")
-		 ;;		 (main_background_color "#ffffff")
-		 ;;		 (main_highlight_color "#0078d7")
-		 ;;		 (main_iSearch_color "#cccccc")
-		 ;;		 (main_hl_line_background_color "#aaaaaa")
-		 ;;		 (main_line_number_color "#555555")
-		 ;;		 (main_line_number_current_line_color "#0078d7")
-
-		 (main_foreground_color "#f3f4f6")
-		 (main_background_color "#111111")
 		 
-		 (main_comment_color "#00aa00")
-		 
-		 (main_highlight_color "#0078d7")
-		 
-		 (main_iSearch_background_color "#5a0000")
-		 (main_iSearch_foreground_color "#e3e4e6")
-		 
-		 (main_line_number_color "#555555")
-		 (main_line_number_current_line_color "#0078d7")
-
-		 (main_highlight_foreground_color "#ffffff")
-		 (main_highlight_background_color "#0078d7")
-
-		 (main_modeline_inactive_background_color "#1c1c1c")
-		 (main_modeline_inactive_foreground_color "#bcbcbc")
-
-		 (main_modeline_active_background_color "#bcbcbc")
-		 (main_modeline_active_foreground_color "#1c1c1c")
-		 
-		 (main_hl_line_background_color "#aaaaaa")
-
-		 (main_dired_directory_background_color "#752000")
-		 (main_dired_directory_foreground_color "#f3f4f6")
+		 (background "#ffffff")
+		 (foreground "#000000")
+		 (keyword_foreground "#ff9900")
+		 (comment_foreground "#008a00")
+		 (highlight_background "#0078d7")
+		 (highlight_foreground "#ffffff")
+		 (modline_active_foreground "#bcbcbc")
+		 (modline_active_background "#1c1c1c")
+		 (modline_inactive_foreground "#1c1c1c")
+		 (modline_inactive_background "#bcbcbc")
+		 (isearch_foreground "#cccccc")
+		 (isearch_background "#1c1c1c")
+		 (line_numbers_foreground "#555555")
+		 (active_line_number_foreground "#aaaaaa")
+		 (hl_line_background "#aaaaaa")
+		 (dired_directory_background "#ff9900")
 		 
 		 )
 
 	(custom-theme-set-faces
 		'loveisanillusion.emacs
 
-		`(default ((t (:foreground ,main_foreground_color :background ,main_background_color))))
-		`(font-lock-comment-face ((t (:foreground ,main_comment_color))))
-		`(font-lock-doc-face ((t (:foreground ,main_foreground_color))))
-		`(font-lock-string-face ((t (:foreground ,main_foreground_color))))
-		`(font-lock-keyword-face ((t (:foreground ,main_foreground_color))))
-		`(font-lock-variable-name-face ((t (:foreground ,main_foreground_color))))
-		`(font-lock-builtin-face ((t (:foreground ,main_foreground_color))))
-		`(font-lock-constant-face ((t (:foreground ,main_foreground_color))))
-	    `(font-lock-number-face ((t (:foreground ,main_foreground_color))))
-		`(font-lock-type-face ((t (:foreground ,main_foreground_color))))
-		`(font-lock-preprocessor-face ((t (:foreground ,main_foreground_color))))
-		`(font-lock-function-name-face ((t (:foreground ,main_foreground_color))))
-     	`(font-lock-function-call-face ((t (:foreground ,main_foreground_color))))
+		`(default ((t (:foreground ,foreground :background ,background))))
+		`(font-lock-comment-face ((t (:foreground ,comment_foreground))))
+		`(font-lock-doc-face ((t (:foreground ,foreground))))
+		`(font-lock-string-face ((t (:foreground ,foreground))))
+		`(font-lock-keyword-face ((t (:foreground ,keyword_foreground))))
+		`(font-lock-variable-name-face ((t (:foreground ,foreground))))
+		`(font-lock-builtin-face ((t (:foreground ,foreground))))
+		`(font-lock-constant-face ((t (:foreground ,foreground))))
+	    `(font-lock-number-face ((t (:foreground ,foreground))))
+		`(font-lock-type-face ((t (:foreground ,foreground))))
+		`(font-lock-preprocessor-face ((t (:foreground ,keyword_foreground))))
+		`(font-lock-function-name-face ((t (:foreground ,foreground))))
+     	`(font-lock-function-call-face ((t (:foreground ,foreground))))
 
-		`(mode-line-inactive ((t (:foreground ,main_modeline_inactive_foreground_color :background ,main_modeline_inactive_background_color))))
-		`(mode-line ((t (:foreground ,main_modeline_active_foreground_color :background ,main_modeline_active_background_color))))
+		`(mode-line-inactive ((t (:foreground ,modline_inactive_foreground :background ,modline_inactive_background))))
+		`(mode-line ((t (:foreground ,modline_inactive_foreground :background ,modline_active_background))))
 
 		`(show-paren-match ((nil )))
         `(show-paren-mismatch ((nil )))
 
-		`(region ((t (:background ,main_highlight_background_color :foreground ,main_highlight_foreground_color))))
+		`(region ((t (:background ,highlight_background :foreground ,highlight_foreground))))
 		
-		`(minibuffer-prompt ((t (:foreground ,main_comment_color))))
+		`(minibuffer-prompt ((t (:foreground ,comment_foreground))))
 
-		`(isearch ((t (:background ,main_iSearch_background_color :foreground ,main_iSearch_foreground_color))))
-		`(query-replace ((t (:background ,main_iSearch_background_color :foreground ,main_iSearch_foreground_color))))
-		`(lazy-highlight ((t (:background ,main_iSearch_background_color :foreground ,main_iSearch_foreground_color))))
-		`(escape-glyph ((t (:background ,main_iSearch_background_color :foreground ,main_iSearch_foreground_color))))
+		`(isearch ((t (:background ,isearch_background :foreground ,isearch_foreground))))
+		`(query-replace ((t (:background ,isearch_background :foreground ,isearch_foreground))))
+		`(lazy-highlight ((t (:background ,isearch_background :foreground ,isearch_foreground))))
+		`(escape-glyph ((t (:background ,isearch_background :foreground ,isearch_foreground))))
 
-		`(dired-directory ((t (:foreground ,main_dired_directory_foreground_color :background ,main_dired_directory_background_color))))
-		`(dired-flagged ((t (:foreground ,main_foreground_color))))
-		`(dired-header ((t (:foreground ,main_foreground_color))))
-		`(dired-ignored ((t (:foreground ,main_foreground_color))))
-		`(dired-marked ((t (:foreground ,main_comment_color))))
-		`(dired-perm-write ((t (:foreground ,main_foreground_color))))
-		`(dired-symlink ((t (:foreground ,main_comment_color))))
-		`(dired-warning ((t (:foreground ,main_foreground_color))))
+		`(dired-directory ((t (:foreground ,foreground :background ,dired_directory_background))))
 		
-		`(highlight ((t (:foreground ,main_foreground_color :background ,main_highlight_color))))
-		`(hl-line ((t (:foreground ,main_foreground_color :background ,main_hl_line_background_color))))
+		`(highlight ((t (:foreground ,highlight_foreground :background ,highlight_background))))
+		`(hl-line ((t (:foreground ,foreground :background ,hl_line_background))))
 		
-		`(line-number ((t (:foreground ,main_line_number_color :background ,main_background_color))))
-		`(line-number-current-line ((t (:foreground ,main_line_number_current_line_color :background ,main_background_color))))
+		`(line-number ((t (:foreground ,line_numbers_foreground :background ,background))))
+		`(line-number-current-line ((t (:foreground ,active_line_number_foreground :background ,background))))
 		
-		`(eshell-prompt ((t (:foreground ,main_foreground_color)))))
+		`(eshell-prompt ((t (:foreground ,comment_foreground)))))
 	)
 
 (provide-theme 'loveisanillusion.emacs)
